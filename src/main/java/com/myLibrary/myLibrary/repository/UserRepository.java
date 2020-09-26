071411ae43a0fc5f6a583e1dadab358c6a082118
Bojan Vujnovic <52334738+vujaaa88@users.noreply.github.com>
2020-09-26 21:04:39 +0200

    Initial commit

--- src/main/java/com/myLibrary/myLibrary/repository/UserRepository.java
+++ src/main/java/com/myLibrary/myLibrary/repository/UserRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.myLibrary.myLibrary.entity.MyUser;
 
 public interface UserRepository extends JpaRepository<MyUser, Long> {
+   
    MyUser findByUserName(String userName);
    MyUser getOne(Long id);
 }
